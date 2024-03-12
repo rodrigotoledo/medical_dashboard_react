92c1d3f6e5c2e74fc97b8fe9668298daa80d5758
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-03-12 20:27:18 -0300

    styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './tailwind.css';
 import App from './App';
+import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_API_HTTP_ADDRESS;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
